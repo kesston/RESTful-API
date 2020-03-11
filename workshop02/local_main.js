28f14b646b4aca18017acbadb1714f736e8c7143
Kesston Tang <kesston@Kesstons-MacBook-Air.local>
2020-03-11 17:19:58 +0800

    workshop02

--- workshop02/local_main.js
+++ workshop02/local_main.js
@@ -18,15 +18,54 @@ app.use(express.urlencoded({ extended: true }));
 
 // Mandatory workshop
 // TODO GET /api/states
-
+app.get("/api/states",
+		(req, resp) => { // handler 
+		 const result = db.findAllStates()
+		// status code
+		resp.status(200)
+		// set Content-Type
+		resp.type("application/json")
+		resp.set("X-generated-on", (new Date()).toLocaleDateString())
+		resp.json(result)
+		}
+)
 
 // TODO GET /api/state/:state
+app.get("/api/states/:state",
+(req, resp) => {
+	const state = req.params.state
+	const limit = parselnt(req.query.limit) || 10;
+	const offset = parseInt(req.query.offset) || 0;
+	const result = db.findCitiesByState (state,
+	{ offset, limit })
+resp.status(200);
+resp.type("application/json")
+resp.json(result)
+}
+)
 
+// TODO DELETE /api/city/:name
 
 // TODO GET /api/city/:cityId
 
 
 // TODO POST /api/city
+app.post(
+		 "/api/city",
+(req, resp) => {
+			const body = req.body;
+			if (db.validateForm(body)){
+			   resp.status(400)
+			   resp.type("application/json")
+			   resp.json({"message": "imcomplete form"})
+			   return
+			}
+			db.insertCity(body)
+			resp.status(201)
+			resp.type("application/json")
+			resp.json({message: "created"})
+}
+)
 
 // Optional workshop
 // TODO HEAD /api/state/:state
@@ -35,7 +74,20 @@ app.use(express.urlencoded({ extended: true }));
 
 
 // TODO GET /state/:state/count
-
+app.get("api/state/:state/count",
+(req, resp) => {
+	 const state = req.params.state
+	 const count =db.countCitiesInState (state)
+	 const result = {
+		 state: state,
+		 numOfCities: count,
+		 timestamp: (new Date()).toLocaleDateString()
+	 }
+resp.status(200);
+resp.type("appliccation/json")
+resp.json(result)
+}
+)
 
 // TODO GET /api/city/:name
 
