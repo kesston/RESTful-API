adea8d3527e047930afe3530d3d0c2fc68bb3727
Kesston Tang <kesston@Kesstons-MacBook-Air.local>
2020-03-13 13:18:41 +0800

    workshop03

--- workshop02/local_main.js
+++ workshop02/local_main.js
@@ -1,5 +1,6 @@
 const range = require('express-range')
 const compression = require('compression')
+const cors = require("cors") 
 
 const express = require('express')
 
@@ -11,6 +12,8 @@ const db = CitiesDB(data);
 
 const app = express();
 
+app.use(cors())
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -19,29 +22,29 @@ app.use(express.urlencoded({ extended: true }));
 // Mandatory workshop
 // TODO GET /api/states
 app.get("/api/states",
-		(req, resp) => { // handler 
-		 const result = db.findAllStates()
+	(req, resp) => { // handler 
+		const result = db.findAllStates()
 		// status code
 		resp.status(200)
 		// set Content-Type
 		resp.type("application/json")
 		resp.set("X-generated-on", (new Date()).toLocaleDateString())
 		resp.json(result)
-		}
+	}
 )
 
 // TODO GET /api/state/:state
 app.get("/api/states/:state",
-(req, resp) => {
-	const state = req.params.state
-	const limit = parselnt(req.query.limit) || 10;
-	const offset = parseInt(req.query.offset) || 0;
-	const result = db.findCitiesByState (state,
-	{ offset, limit })
-resp.status(200);
-resp.type("application/json")
-resp.json(result)
-}
+	(req, resp) => {
+		const state = req.params.state
+		const limit = parselnt(req.query.limit) || 10;
+		const offset = parseInt(req.query.offset) || 0;
+		const result = db.findCitiesByState(state,
+			{ offset, limit })
+		resp.status(200);
+		resp.type("application/json")
+		resp.json(result)
+	}
 )
 
 // TODO DELETE /api/city/:name
@@ -51,20 +54,20 @@ resp.json(result)
 
 // TODO POST /api/city
 app.post(
-		 "/api/city",
-(req, resp) => {
-			const body = req.body;
-			if (db.validateForm(body)){
-			   resp.status(400)
-			   resp.type("application/json")
-			   resp.json({"message": "imcomplete form"})
-			   return
-			}
-			db.insertCity(body)
-			resp.status(201)
+	"/api/city",
+	(req, resp) => {
+		const body = req.body;
+		if (db.validateForm(body)) {
+			resp.status(400)
 			resp.type("application/json")
-			resp.json({message: "created"})
-}
+			resp.json({ "message": "imcomplete form" })
+			return
+		}
+		db.insertCity(body)
+		resp.status(201)
+		resp.type("application/json")
+		resp.json({ message: "created" })
+	}
 )
 
 // Optional workshop
@@ -75,22 +78,30 @@ app.post(
 
 // TODO GET /state/:state/count
 app.get("api/state/:state/count",
-(req, resp) => {
-	 const state = req.params.state
-	 const count =db.countCitiesInState (state)
-	 const result = {
-		 state: state,
-		 numOfCities: count,
-		 timestamp: (new Date()).toLocaleDateString()
-	 }
-resp.status(200);
-resp.type("appliccation/json")
-resp.json(result)
-}
+	(req, resp) => {
+		const state = req.params.state
+		const count = db.countCitiesInState(state)
+		const result = {
+			state: state,
+			numOfCities: count,
+			timestamp: (new Date()).toLocaleDateString()
+		}
+		resp.status(200);
+		resp.type("appliccation/json")
+		resp.json(result)
+	}
 )
 
 // TODO GET /api/city/:name
-
+app.get("/api/city/:name",
+	(req, resp) => {
+		const state = req.params.state
+		const result = db.findCitiesByName
+		resp.status(200);
+		resp.type("application/json")
+		resp.json(result)
+	}
+)
 
 // End of workshop
 
--- workshop03/local_main.js
+++ workshop03/local_main.js
@@ -6,7 +6,7 @@ const range = require('express-range')
 const compression = require('compression')
 
 const { Validator, ValidationError } = require('express-json-validator-middleware')
-const  OpenAPIValidator  = require('express-openapi-validator').OpenApiValidator;
+const OpenAPIValidator = require('express-openapi-validator').OpenApiValidator;
 
 const schemaValidator = new Validator({ allErrors: true, verbose: true });
 
@@ -30,33 +30,119 @@ app.use(express.urlencoded({ extended: true }));
 // Start of workshop
 
 // TODO 1/2 Load schemans
+new OpenAPIValidator({
+    apiSpec: join(__dirname, "schema", "zips.ymal"
+}).install(app) 
+    .then(() => {
+        // proceed with the rest of our map
+        app.use('schema', express.static(join(__dirname, "schema")));
 
+        app.use((error, req, resp, next) => {
+
+            if (error instanceof ValidationError) {
+                console.error('Schema validation error: ', error)
+                return resp.status(400).type('application/json').json({ error: error });
+            }
+
+            else if (error.status) {
+                console.error('OpenAPI specification error: ', error)
+                return resp.status(400).type('application/json').json({ error: error });
+            }
+
+            console.error('Error: ', error);
+            resp.status(400).type('application/json').json({ error: error });
+
+        });
+
+        const PORT = parseInt(process.argv[2] || process.env.APP_PORT) || 3000;
+        app.listen(PORT, () => {
+            console.info(`Application started on port ${PORT} at ${new Date()}`);
+        });
+
+    }
+
+    )
 
 // Start of workshop
 // TODO 2/2 Copy your routes from workshop02 here
+// TODO GET /api/states
+app.get("/api/states",
+    (req, resp) => { // handler 
+        const result = db.findAllStates()
+        // status code
+        resp.status(200)
+        // set Content-Type
+        resp.type("application/json")
+        resp.set("X-generated-on", (new Date()).toLocaleDateString())
+        resp.json(result)
+    }
+)
 
+// TODO GET /api/state/:state
+app.get("/api/states/:state",
+    (req, resp) => {
+        const state = req.params.state
+        const limit = parselnt(req.query.limit) || 10;
+        const offset = parseInt(req.query.offset) || 0;
+        const result = db.findCitiesByState(state,
+            { offset, limit })
+        resp.status(200);
+        resp.type("application/json")
+        resp.json(result)
+    }
+)
+
+// TODO GET /api/city/:cityId
+
+
+// TODO POST /api/city
+app.post(
+    "/api/city",
+    (req, resp) => {
+        const body = req.body;
+        if (db.validateForm(body)) {
+            resp.status(400)
+            resp.type("application/json")
+            resp.json({ "message": "imcomplete form" })
+            return
+        }
+        db.insertCity(body)
+        resp.status(201)
+        resp.type("application/json")
+        resp.json({ message: "created" })
+    }
+)
+
+// Optional workshop
+// TODO HEAD /api/state/:state
+// IMPORTANT: HEAD must be place before GET for the
+// same resource. Otherwise the GET handler will be invoked
+
+
+// TODO GET /state/:state/count
+app.get("api/state/:state/count",
+    (req, resp) => {
+        const state = req.params.state
+        const count = db.countCitiesInState(state)
+        const result = {
+            state: state,
+            numOfCities: count,
+            timestamp: (new Date()).toLocaleDateString()
+        }
+        resp.status(200);
+        resp.type("appliccation/json")
+        resp.json(result)
+    }
+)
+
+// TODO GET /api/city/:name
+app.get("/api/city/:name",
+    (req, resp) => {
+        const state = req.params.state
+        const result = db.findCitiesByName
+        resp.status(200);
+        resp.type("application/json")
+        resp.json(result)
+    }
+)
 // End of workshop
-
-app.use('/schema', express.static(join(__dirname, 'schema')));
-
-app.use((error, req, resp, next) => {
-
-    if (error instanceof ValidationError) {
-  		console.error('Schema validation error: ', error)
-  		return resp.status(400).type('application/json').json({ error: error });
-    }
-
-    else if (error.status) {
-  		console.error('OpenAPI specification error: ', error)
-  		return resp.status(400).type('application/json').json({ error: error });
-    }
-
-    console.error('Error: ', error);
-    resp.status(400).type('application/json').json({ error: error });
-
-});
-
-const PORT = parseInt(process.argv[2] || process.env.APP_PORT) || 3000;
-app.listen(PORT, () => {
-    console.info(`Application started on port ${PORT} at ${new Date()}`);
-});
